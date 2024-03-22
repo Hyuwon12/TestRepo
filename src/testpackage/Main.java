429766dd91e00f2651b1e44e99cedaaf58d732b4
sonhyeong-won <hyuwon12@gmail.com>
2024-03-22 20:03:30 +0900

    test

--- src/testpackage/Main.java
+++ src/testpackage/Main.java
@@ -6,11 +6,6 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        int a=1;
-        int b=2;
-        System.out.printf("%d + %d = %d",a,b,a+b);
-        String s =null;
-        String.valueOf(a);
-        Integer.parseInt()
+
     }
 }
